e69bae9d5e74a364fa6cb16d87bfe1cda0a7e918
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2021-08-27 15:36:43 -0300

    Primeiro commit. Sistema de controle de ponto de acesso.

---
+++ src/main/java/com/dio/live/model/JornadaTrabalho.java
@@ -0,0 +1,26 @@
+package com.dio.live.model;
+
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+@Audited
+public class JornadaTrabalho {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String descricao;
+}
